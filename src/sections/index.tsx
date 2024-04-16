c242c4242c13bedcb5d2a3bb8db3ecf2725917fc
Vlad Kupnyy <47388945+Odinwan@users.noreply.github.com>
2024-04-16 12:26:26 +0300

    [wip]

--- src/sections/index.tsx
+++ src/sections/index.tsx
@@ -2,29 +2,29 @@ import HubIcon from '@mui/icons-material/Hub';
 import IconButton from '@mui/material/IconButton';
 import MobXBody from '@sections/components/MobXBody';
 import RxBody from '@sections/components/RxXBody';
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 import Container from './StyledComponents';
 
 const App = () => {
-  const [isMobX, setIsMobX] = useState<boolean>(true);
-  const handleSwitchState = () => setIsMobX((current) => !current);
+    const [isMobX, setIsMobX] = useState<boolean>(true);
+    const handleSwitchState = () => setIsMobX((current) => !current);
 
-  return (
-    <Container>
-      <div className="calculator">
-        <div className="header">
-          {isMobX ? 'MobX state' : 'RX state'}
-          <IconButton onClick={handleSwitchState}>
-            <HubIcon />
-          </IconButton>
-        </div>
-        {isMobX && <MobXBody />}
-        {!isMobX && <RxBody />}
-        <div className="footer" />
-      </div>
-    </Container>
-  );
+    return (
+        <Container>
+            <div className="calculator">
+                <div className="header">
+                    {isMobX ? 'MobX state' : 'RX state'}
+                    <IconButton onClick={handleSwitchState}>
+                        <HubIcon/>
+                    </IconButton>
+                </div>
+                {isMobX && <MobXBody/>}
+                {!isMobX && <RxBody/>}
+                <div className="footer"/>
+            </div>
+        </Container>
+    );
 };
 
 export default App;
